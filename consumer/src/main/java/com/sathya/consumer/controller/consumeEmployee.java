d862916528e6b64f22ba03d34d8506bfde0762a2
sathya-cherrygrove <sat.headingly@gmail.com>
2018-08-14 18:55:23 +0530

    added eureka ribbon zuul

--- consumer/src/main/java/com/sathya/consumer/controller/consumeEmployee.java
+++ consumer/src/main/java/com/sathya/consumer/controller/consumeEmployee.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -21,6 +22,9 @@ public class consumeEmployee {
 
 	@Autowired
 	private DiscoveryClient discoveryclient;
+
+	@Autowired
+	private LoadBalancerClient loadBalancer;
 
 	@RequestMapping("/consumeProducer")
 	public String consumeEmployee() {
@@ -38,20 +42,58 @@ public class consumeEmployee {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = null;
 		try {
-			response = restTemplate.exchange(baseUrl+"/getEmployee", HttpMethod.GET, getHeaders(), String.class);
+			response = restTemplate.exchange(baseUrl + "/getEmployee", HttpMethod.GET, getHeaders(), String.class);
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
-		
 
-		 return response.getBody();
-		//return "Size of the List is : "+ Integer.toString(services.size());
+		return response.getBody();
+		// return "Size of the List is : "+ Integer.toString(services.size());
+	}
+
+	@RequestMapping("/loadConsumeProducer")
+	public String loadConsumeProducer() {
+		ServiceInstance serviceInstance = loadBalancer.choose("producer");
+		System.out.println("URI : " + serviceInstance.getUri());
+		System.out.println("Host : " + serviceInstance.getHost());
+		System.out.println("Service Id : " + serviceInstance.getServiceId());
+		String baseUrl = serviceInstance.getUri().toString();
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = null;
+		try {
+			response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(), String.class);
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		return response.getBody();
+
+	}
+
+	@RequestMapping("/talkToZuul")
+	public String talkToZuul() {
+		System.out.println("Talk to Zuul Called");
+		List<ServiceInstance> instances = discoveryclient.getInstances("netflix-zuul");
+		ServiceInstance serviceInstance = instances.get(0);
+		System.out.println("Calling Service : " + serviceInstance.getUri());
+		String baseUrl = serviceInstance.getUri().toString();
+		baseUrl = baseUrl + "/producer/getEmployee";
+		System.out.println("Base Url : " + baseUrl);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = null;
+		try {
+			response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(), String.class);
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		return response.getBody();
 	}
 
 	private static HttpEntity<?> getHeaders() throws IOException {
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 		return new HttpEntity<>(headers);
+
 	}
 
 }
--- producer/src/main/java/com/sathya/producer/controller/EmployeeService.java
+++ producer/src/main/java/com/sathya/producer/controller/EmployeeService.java
@@ -2,14 +2,25 @@ package com.sathya.producer.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class EmployeeService {
 
 	@RequestMapping("/getEmployee")
-	public Employee getEmployee() {
+	@HystrixCommand(fallbackMethod = "fallback")
+	public Employee getEmployee() throws Exception {
 		Employee employee = new Employee("sathya", "software engineer", 10000);
 		return employee;
+
+	}
+
+	public Employee fallback() {
+		Employee emp = new Employee();
+		emp.setName("Fallback Name");
+		emp.setDesignation("boss man");
+		emp.setSal(20000);
+		return emp;
 
 	}
 
